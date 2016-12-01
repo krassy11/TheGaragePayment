dd57ea7dc6b0d2298efd76b082ca47038335e987
Admin <Admin@ADMIN.COM>
2016-12-01 16:40:56 +0200

    fff

--- source/TheGaragePayment/WebApiTest/Controllers/ValuesController.cs
+++ source/TheGaragePayment/WebApiTest/Controllers/ValuesController.cs
@@ -39,11 +39,6 @@ namespace WebApiTest.Controllers
             return response;
         }
 
-        public IHttpActionResult Add([FromBody]Animal name)
-        {
-            return this.Ok();
-        }
-
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
